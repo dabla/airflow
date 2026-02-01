2c2164a6e34f8358e730a7869cfa45c132abed03
David Blain <info@dabla.be>
2026-02-01 12:10:08 +0100

    refactor: Fixed typing of rows parameter in _insert_rows method

--- providers/common/sql/src/airflow/providers/common/sql/operators/sql.py
+++ providers/common/sql/src/airflow/providers/common/sql/operators/sql.py
@@ -1395,7 +1395,7 @@ class SQLInsertRowsOperator(BaseSQLOperator):
             return [column for column in self.columns if column not in self.ignored_columns]
         return self.columns
 
-    def _insert_rows(self, rows: list[Any], context: Context):
+    def _insert_rows(self, rows: list[Any] | XComArg, context: Context):
         if self._rows_processor:
             rows = self._rows_processor(rows, **context)  # type: ignore
 
