b18e9420ddca7dfe4d1dd91b7725c8f9bd2f8512
David Blain <info@dabla.be>
2026-02-11 10:47:59 +0100

    refactor: Added start_from_trigger property on Trigger

--- airflow-core/src/airflow/jobs/triggerer_job_runner.py
+++ airflow-core/src/airflow/jobs/triggerer_job_runner.py
@@ -671,11 +671,7 @@ class TriggerRunnerSupervisor(WatchedSubprocess):
         # When a TaskInstance of a Trigger contains start_trigger_args and has a task with templated fields,
         # it means we need to load the SerializedDagModel so we can build a RuntimeTaskInstance later on which
         # will allow us to build a context on which we will render the templated fields.
-        if (
-            trigger.task_instance.task and
-            trigger.task_instance.task.template_fields and
-            trigger.task_instance.start_trigger_args
-        ):
+        if trigger.start_from_trigger:
             serialized_dag_model = dag_bag.get_dag_model(
                 version_id=trigger.task_instance.dag_version_id,
                 session=session,
