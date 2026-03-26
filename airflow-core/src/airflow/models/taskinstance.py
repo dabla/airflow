aec736c22d7db03cbc839493487295acbf1d0ba9
David Blain <info@dabla.be>
2026-03-26 13:52:39 +0100

    refactor: Fixed indentation of defer_task method in TaskInstance

--- airflow-core/src/airflow/models/taskinstance.py
+++ airflow-core/src/airflow/models/taskinstance.py
@@ -1616,61 +1616,60 @@ class TaskInstance(Base, LoggingMixin, BaseWorkload):
             assert self.start_date
             assert isinstance(self.task, Operator)
 
-            # Remaining task expansion still running from previous triggerer so reschedule
-            if self.context_carrier and "trigger" in self.context_carrier:
-                trigger_classpath, trigger_kwargs = self.context_carrier.pop("trigger", (None, None))
+        # Remaining task expansion still running from previous triggerer so reschedule
+        if self.context_carrier and "trigger" in self.context_carrier:
+            trigger_classpath, trigger_kwargs = self.context_carrier.pop("trigger", (None, None))
 
-                self.log.info(
-                    "Creating trigger from context_carrier for task_id %s: %s", self.task_id, trigger_kwargs
-                )
-                trigger_row = Trigger(
-                    classpath=trigger_classpath,
-                    kwargs=trigger_kwargs or {},
-                )
-            elif not (not self.next_trigger_id and (start_trigger_args := self.start_trigger_args)):
-                # self.log.warning("Couldn't create trigger from start_from_trigger for task_id %s thus could not be deferred!", self.task_id)
-                return False
+            self.log.info(
+                "Creating trigger from context_carrier for task_id %s: %s", self.task_id, trigger_kwargs
+            )
+            trigger_row = Trigger(
+                classpath=trigger_classpath,
+                kwargs=trigger_kwargs or {},
+            )
+        elif not (not self.next_trigger_id and (start_trigger_args := self.start_trigger_args)):
+            # self.log.warning("Couldn't create trigger from start_from_trigger for task_id %s thus could not be deferred!", self.task_id)
+            return False
+        else:
+            trigger_kwargs = start_trigger_args.trigger_kwargs or {}
+            timeout = start_trigger_args.timeout
+
+            # Calculate timeout too if it was passed
+            if timeout is not None:
+                self.trigger_timeout = timezone.utcnow() + timeout
             else:
-                trigger_kwargs = start_trigger_args.trigger_kwargs or {}
-                timeout = start_trigger_args.timeout
+                self.trigger_timeout = None
 
-                # Calculate timeout too if it was passed
-                if timeout is not None:
-                    self.trigger_timeout = timezone.utcnow() + timeout
-                else:
-                    self.trigger_timeout = None
+            trigger_row = Trigger(
+                classpath=start_trigger_args.trigger_cls,
+                kwargs=trigger_kwargs,
+            )
 
-                trigger_row = Trigger(
-                    classpath=start_trigger_args.trigger_cls,
-                    kwargs=trigger_kwargs,
-                )
+        # First, make the trigger entry
+        session.add(trigger_row)
+        session.flush()
 
-            # First, make the trigger entry
-            session.add(trigger_row)
-            session.flush()
+        # Then, update ourselves so it matches the deferral request
+        # Keep an eye on the logic in `check_and_change_state_before_execution()`
+        # depending on self.next_method semantics
+        self.state = TaskInstanceState.DEFERRED
+        self.trigger_id = trigger_row.id
+        self.next_method = start_trigger_args.next_method
+        self.next_kwargs = start_trigger_args.next_kwargs or {}
 
-            # Then, update ourselves so it matches the deferral request
-            # Keep an eye on the logic in `check_and_change_state_before_execution()`
-            # depending on self.next_method semantics
-            self.state = TaskInstanceState.DEFERRED
-            self.trigger_id = trigger_row.id
-            self.next_method = start_trigger_args.next_method
-            self.next_kwargs = start_trigger_args.next_kwargs or {}
-
-            # If an execution_timeout is set, set the timeout to the minimum of
-            # it and the trigger timeout
-            if execution_timeout := self.task.execution_timeout:
-                if self.trigger_timeout:
-                    self.trigger_timeout = min(self.start_date + execution_timeout, self.trigger_timeout)
-                else:
-                    self.trigger_timeout = self.start_date + execution_timeout
-            self.start_date = timezone.utcnow()
-            if self.state != TaskInstanceState.UP_FOR_RESCHEDULE:
-                self.try_number += 1
-            if self.test_mode:
-                _add_log(event=self.state, task_instance=self, session=session)
-            return True
-        return False
+        # If an execution_timeout is set, set the timeout to the minimum of
+        # it and the trigger timeout
+        if execution_timeout := self.task.execution_timeout:
+            if self.trigger_timeout:
+                self.trigger_timeout = min(self.start_date + execution_timeout, self.trigger_timeout)
+            else:
+                self.trigger_timeout = self.start_date + execution_timeout
+        self.start_date = timezone.utcnow()
+        if self.state != TaskInstanceState.UP_FOR_RESCHEDULE:
+            self.try_number += 1
+        if self.test_mode:
+            _add_log(event=self.state, task_instance=self, session=session)
+        return True
 
     @classmethod
     def fetch_handle_failure_context(
